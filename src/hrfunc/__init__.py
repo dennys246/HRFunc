57ddcc8ca83bf71b605d3ce2e9c68facffbca39d
Denny Schaedig <dennys@wustl.edu>
2025-05-27 14:11:33 -0600

    Updating distribution to reflect new version

--- src/hrfunc/__init__.py
+++ src/hrfunc/__init__.py
@@ -1,6 +1,7 @@
 # src/hrfunc/__init__.py
 
-from .hrfunc import montage, estimate_hrfs, localize_hrfs  # or Tool
+from .hrfunc import montage, estimate_hrfs, locate_hrfs  # or Tool
+from .hrtree import tree
 
 # Define what's public
-__all__ = ["montage", "estimate_hrfs", "localize_hrfs", "tree", "hashtable"]
+__all__ = ["montage", "estimate_hrfs", "locate_hrfs", "tree"]
