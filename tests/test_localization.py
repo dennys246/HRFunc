1fc545e25a9954e0f5392605f7c35c8ac0479aea
Denny Schaedig <dennys@wustl.edu>
2025-08-11 23:18:51 -0600

    Adding in tests for making sure the HRF localization function is working properly and finding nearby HRFs, alongside a test to ensure the nearest neightbor functions dont find HRFs they shouldnt (i.e. Nodes outside of minimum distance requirements)

---
+++ tests/test_localization.py
@@ -0,0 +1,37 @@
+import pytest, mne, os, time
+import test_loading as loader
+import hrfunc as hrf
+from pathlib import Path
+
+print("TEST: Assessing HRF localization capabilities...")
+
+# Grab test data directory
+TEST_DATA_DIR = Path(__file__).parent / "data"
+
+for filetype_load, filetype in zip([loader.load_nirx, loader.load_snirf, loader.load_fif], ['NIRX', 'SNIRF', 'FIF']):
+    print(f"Testing {filetype} files")
+    scans = filetype_load()
+
+    # Localize HRFs
+    print(f"Localizing true HRFs...")
+    montage = hrf.localize_hrfs(scans[0])
+
+    for ch_name in montage.channels.keys():
+        print(ch_name)
+
+    del montage
+
+    print("Attempting to localize sham HRFs, loading new montage...")
+    montage = hrf.montage(scans[0])
+
+    # Load HRFs with sham locations
+    print(f"Loading sham HRFs into HbO and HbR tree")
+    montage.hbo_tree = hrf.tree(f"{TEST_DATA_DIR}/sham_hbo_hrfs.json")
+    montage.hbr_tree = hrf.tree(f"{TEST_DATA_DIR}/sham_hbr_hrfs.json")
+
+    montage.localize_hrfs(max_distance = 0.001)
+
+    for ch_name in montage.channels.keys():
+        print(ch_name)
+
+    del montage
