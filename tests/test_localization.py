6b394bd520da73bf780805352619ae46764ae6b5
Denny Schaedig <dennys@wustl.edu>
2025-08-15 14:00:10 -0600

    Adding in the sham portion and adding in a raise error component to make sure the script crashes on spotting a failed test in the sham and real HRF localizations

--- tests/test_localization.py
+++ tests/test_localization.py
@@ -25,21 +25,29 @@ for filetype_load, filetype in zip([loader.load_nirx, loader.load_snirf, loader.
 
     # Localize HRFs with function call and true HRtree
     print(f"Localizing true HRFs...")
-    montage = hrf.localize_hrfs(scans[0], 0.1, verbose = True)
+    montage = hrf.localize_hrfs(scans[0], 0.001, verbose = True)
+
+    # Check that all channels were found
+    for ch_name in montage.channels.keys():
+        if montage.channels[ch_name].context['method'] == 'canonical':
+            RuntimeError(f"ERROR: True channel {ch_name} not found in localiztion test")
 
     del montage
 
     #print("Attempting to localize sham HRFs, loading new montage...")
-    #montage = hrf.montage(scans[0])
+    montage = hrf.montage(scans[0])
 
     # Load HRFs with sham locations
-    #print(f"Loading sham HRFs into HbO and HbR tree")
-    #montage.hbo_tree = hrf.tree(f"{TEST_DATA_DIR}/sham_hbo_hrtree.json")
-    #montage.hbr_tree = hrf.tree(f"{TEST_DATA_DIR}/sham_hbr_hrtree.json")
+    print(f"Loading sham edge-case HRFs into HbO and HbR tree")
+    montage.hbo_tree = hrf.tree(f"{TEST_DATA_DIR}/hrfs/sham_hbo_hrtree.json")
+    montage.hbr_tree = hrf.tree(f"{TEST_DATA_DIR}/hrfs/sham_hbr_hrtree.json")
 
-    #montage.localize_hrfs(max_distance = 0.001)
+    montage.localize_hrfs(max_distance = 0.001, verbose = True)
 
-    #for ch_name in montage.channels.keys():
-    #    print(ch_name)
+    # Check that all channels were found
+    success = True
+    for ch_name in montage.channels.keys():
+        if montage.channels[ch_name].context['method'] != 'canonical':
+            RuntimeError(f"ERROR: Sham localization found an HRF, sham localization failed to fail and rely on canonical")
 
-    #del montage
+    del montage
